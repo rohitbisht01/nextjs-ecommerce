62ffc501b346a5d8fc1c083b8e5ac52d19305249
rohitbisht01 <rb9519208@gmail.com>
2023-10-30 20:12:22 +0530

    implemented added to cart functionality and working on navbar for the app

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,66 @@
+import PriceTag from "@/app/components/PriceTag";
+import NotFoundPage from "@/app/not-found";
+import { prisma } from "@/lib/db/prisma";
+import { Metadata } from "next";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { title } from "process";
+import { cache } from "react";
+import AddToCartButton from "./AddToCartButton";
+import { incrementProductQuantity } from "./actions";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (!product) notFound();
+
+  return product;
+});
+
+export async function generateMetadata({
+  params: { id },
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProduct(id);
+
+  return {
+    title: product.name + " - ShopSwift",
+    description: product.description,
+    openGraph: {
+      images: [{ url: product.imageUrl }],
+    },
+  };
+}
+
+export default async function ProductPage({
+  params: { id },
+}: ProductPageProps) {
+  const product = await getProduct(id);
+
+  return (
+    <div className="flex flex-col lg:flex-row gap-4 lg:items-center">
+      <Image
+        src={product?.imageUrl}
+        alt={product?.name}
+        width={500}
+        height={500}
+        priority
+        className="rounded-lg"
+      />
+
+      <div>
+        <h1 className="text-5xl font-bold">{product?.name}</h1>
+        <PriceTag price={product?.price} className="mt-4" />
+        <p className="py-6">{product?.description}</p>
+        <AddToCartButton
+          productId={product.id}
+          incrementProductQuantity={incrementProductQuantity}
+        />
+      </div>
+    </div>
+  );
+}
